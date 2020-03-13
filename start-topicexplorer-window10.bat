7230b89aa4fd25632a32b385431dcd5a21694d28
Alexander Hinneburg <alexander.hinneburg@informatik.uni-halle.de>
2020-03-13 15:32:04 +0100

    refactor docker-compose files #2
    - remove reduncancy in compose files
    - remove prod.yml
    - use override.yml for building
    - use named volume for windows, webapps are not working otherwise
    - refactor start scripts

--- start-topicexplorer-window10.bat
+++ start-topicexplorer-window10.bat
@@ -1,2 +1,2 @@
-start docker-compose -f docker-compose.prod.yml -f docker-compose-windows10-hyperV.override.yml up --no-build
+start docker-compose -f docker-compose.yml -f docker-compose-windows10-hyperV.override.yml up --no-build
 exit
